a9d27e3648f88d05bb81f5faebafa65cad0cb9a5
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-03-12 15:24:18 +0100

    feat(): stripe install et debut middleware

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from models.data import db, Contribution
+from middleware import Middleware
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+# app.wsgi_app = Middleware(app.wsgi_app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
+
 def get_contributions():
     return Contribution.query.all()
 
+
 # ROUTES
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     contributions = get_contributions()
     return render_template("index.html", contributions=contributions)
 
-@app.route("/a-propos")
+
+@app.route("/a-propos", methods=["GET"])
 def about():
     return render_template("about.html")
 
-@app.route("/nos-prix")
+
+@app.route("/nos-prix", methods=["GET"])
 def pricing():
     return render_template("pricing.html")
+
+
+@app.route("/inscription", methods=["GET"])
+def register():
+    pass
+
+
+@app.route("/connexion", methods=["GET"])
+def login():
+    pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
